ee8d4f7900c9dedea92dc5a34d56db067ec095ed
Rita Leppiniemi <ritaleppiniemi@gmail.com>
2023-01-31 13:43:54 +0200

    Initial Commit

---
+++ 3.Github/__GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  __GithubApp.swift
+//  3.Github
+//
+//  Created by Rita Leppiniemi on 31.1.2023.
+//
+
+import SwiftUI
+
+@main
+struct __GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
